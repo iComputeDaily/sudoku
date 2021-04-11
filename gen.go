f11451c134acaa36970f16ec65f7bc37b7c81e03
iComputeDaily <henry.haines@thetwelfth.com>
2021-04-11 11:09:00 -0400

    Fallback just in case

--- gen.go
+++ gen.go
@@ -16,23 +16,16 @@ func (b *SquareBoard) Generate() {
 // calls itself on the next cell recursivly. Returns true if it
 // reaches the last cell, and false if it reaches a dead end.
 func (b *SquareBoard) tryNums(x, y int) bool {
-	// Make a randomized list on numbers
-	numList := make([]int, 9)
-	for i := 0; i < len(numList); i++ {
-		numList[i] = i + 1
-	}
+	// Make a randomized list of posibilities
+	numList := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
 	rand.Shuffle(len(numList), func(x, y int) { numList[x], numList[y] = numList[y], numList[x] })
 
 	// Try all nums
 	for _, num := range numList {
 		b[x][y] = num
 
-		if b.check() {
+		if b.check() { // Solution not yet found
 			switch {
-			// Reached the end
-			case x == 8 && y == 8:
-				return true
-
 			// Continue on current line
 			case x < 8:
 				if b.tryNums(x+1, y) {
@@ -52,3 +45,5 @@ func (b *SquareBoard) tryNums(x, y int) bool {
 	b[x][y] = 0
 	return false
 }
+
+// removeNums trys remov
--- solve.go
+++ solve.go
@@ -1,17 +1,27 @@
 package main
 
+import (
+// "math/rand"
+)
+
 // checkDuplicates returns false if there are duplicates, and true if there aren't.
-func (g Group) checkDuplicates() bool {
-	numTable := make(map[int]bool)
+func (g Group) check() bool {
+	numIndex := make(map[int]bool)
 
 	for i := 0; i < len(g); i++ {
-		_, exists := numTable[*g[i]]
+		// If number seen it isn't a solution
+		_, exists := numIndex[*g[i]]
 		if exists {
 			return false
 		}
-		if *g[i] != 0 {
-			numTable[*g[i]] = true
+
+		// If zero sulution not found
+		if *g[i] == 0 {
+			return false
 		}
+
+		// Mark number as seen
+		numIndex[*g[i]] = true
 	}
 
 	return true
@@ -22,10 +32,39 @@ func (b *SquareBoard) check() bool {
 	for i := 0; i < 27; i++ {
 		g := b.Group(i)
 
-		if !g.checkDuplicates() {
+		if !g.check() {
 			return false
 		}
 	}
 
 	return true
+}
+
+// trySolution recursivly checks cells for a solution, bactracting until all are checked
+func (b *SquareBoard) trySolution(x, y, numSolutions int) bool {
+	if b[x][y] != 0 {
+		switch {
+		// Reached the end
+		case b.check():
+			if numSolutions+1 >= 2 {
+				return true
+			}
+
+		// Continue on current line
+		case x < 8:
+			return b.trySolution(x+1, y, numSolutions)
+
+		// Move to next line
+		case x == 8:
+			return b.trySolution(0, y+1, numSolutions)
+		}
+	}
+
+	// Make a list of posibilities
+	numList := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
+
+	for _, num := range numList {
+		b[x][y] = num
+
+	}
 }
